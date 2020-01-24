898a79e4fedba813586783c3f2eadc981414c7d9
Nelson Foltz <nelsfoltz@gmail.com>
2020-01-24 16:34:46 -0500

    formatted date with date pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string;
-  date: string;
+  selectedDate: string;
 
   onNameChange(value: string) {
     this.name = value;
   }
 
   onDateChange(value: string) {
-    this.date = value;
+    this.selectedDate = value;
   }
 }
